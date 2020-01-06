4738297e61d915033b52867f812fa7db88a61a2a
Michal Faron <michal@arronpartners.com>
2020-01-06 21:59:08 +0100

    _

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import numpy as np
 
-k =
+df = pd.DataFrame([[12, 25, 2019, 10], [1, 15, 2020, 11]], columns=["month", "day", "year", "hour"])
+pd.to_datetime(df[["month", "day", "year"]])
+df.index = pd.to_datetime(df[["month", "day", "year"]])
+
+
+print(df)
